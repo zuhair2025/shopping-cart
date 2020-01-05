639cf8d05d3dc6ab22eb7035ef362150c559a2bb
zuhair <zuhair2025@gmail.com>
2020-01-05 18:25:00 +0600

    added this folder

---
+++ resources/views/shop/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+@section('title','Laravel Shopping Cart')
+@section('content')
+<div class="container">
+    <div class="row">
+        <h1>It Works!</h1>
+    </div>
+</div>
+@endsection
