e774644e1961e68c4769dc6b1d8b5d1727b8b075
zuhair <zuhair2025@gmail.com>
2020-01-06 09:51:34 +0600

    changes in product view

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+    <a class="navbar-brand" href="#">Hidden brand</a>
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </li>
+    </ul>
+    <ul class="form-inline navbar-nav dropdown my-2 my-lg-0">
+    <li class="nav-item"> 
+    <a href="#" class="nav-link"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Shoppin Cart</a></li>
+    <li class="nav-item dropdown my-2 my-lg-0">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <i class="fa fa-user" aria-hidden="true"></i> User Account
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+    </ul>
+    
+  </div>
+</nav>
--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -1,9 +1,79 @@
 @extends('layouts.master')
 @section('title','Laravel Shopping Cart')
 @section('content')
-<div class="container">
-    <div class="row">
-        <h1>It Works!</h1>
-    </div>
+<div class="row" style="margin-top:20px">
+
+<div class="col-md-4" style="margin-bottom:20px">
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h3 class="card-title">Product Title</h3>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <p class="float-left font-weight-bold">$12</p>
+    <button type="button" class="btn btn-success float-right">Add to Cart</button>
+  </div>
+</div>
+</div>
+
+<div class="col-md-4" style="margin-bottom:20px">
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h3 class="card-title">Product Title</h3>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <p class="float-left font-weight-bold">$12</p>
+    <button type="button" class="btn btn-success float-right">Add to Cart</button>
+  </div>
+</div>
+</div>
+
+<div class="col-md-4" style="margin-bottom:20px">
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h3 class="card-title">Product Title</h3>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <p class="float-left font-weight-bold">$12</p>
+    <button type="button" class="btn btn-success float-right">Add to Cart</button>
+  </div>
+</div>
+</div>
+
+<div class="col-md-4" style="margin-bottom:20px">
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h3 class="card-title">Product Title</h3>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <p class="float-left font-weight-bold">$12</p>
+    <button type="button" class="btn btn-success float-right">Add to Cart</button>
+  </div>
+</div>
+</div>
+
+<div class="col-md-4" style="margin-bottom:20px">
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h3 class="card-title">Product Title</h3>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <p class="float-left font-weight-bold">$12</p>
+    <button type="button" class="btn btn-success float-right">Add to Cart</button>
+  </div>
+</div>
+</div>
+
+<div class="col-md-4" style="margin-bottom:20px">
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h3 class="card-title">Product Title</h3>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <p class="float-left font-weight-bold">$12</p>
+    <button type="button" class="btn btn-success float-right">Add to Cart</button>
+  </div>
+</div>
+</div>
+
 </div>
 @endsection
